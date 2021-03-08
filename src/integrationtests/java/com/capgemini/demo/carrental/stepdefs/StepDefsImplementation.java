b90227cf201d7cb992df97a6c93b3531b5b76164
MABOBROW <maciej.bobrowski@capgemini.com>
2021-03-08 08:48:14 +0100

    First scenario steps and implementation.

--- src/integrationtests/java/com/capgemini/demo/carrental/stepdefs/StepDefsImplementation.java
+++ src/integrationtests/java/com/capgemini/demo/carrental/stepdefs/StepDefsImplementation.java
@@ -1,6 +1,9 @@
 package com.capgemini.demo.carrental.stepdefs;
 
 import com.capgemini.demo.carrental.config.StepDefsConfig;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.Assert;
 import org.slf4j.Logger;
@@ -28,44 +31,44 @@ public class StepDefsImplementation {
     @Autowired
     private CommonUtility commonUtility = new CommonUtility();
 
-//    @Given("the REST service with initial car data id {string} is available and the {string} method is supported")
-    public void the_REST_service_with_initial_car_data_id_is_available_and_the_method_is_supported(String id, String httpMethod) {
+    @Given("the REST service with initial car data id {string} is available and the {string} method is supported")
+    public void the_rest_service_with_initial_car_data_id_is_available_and_the_method_is_supported(String id, String httpMethod) {
         requestType = HttpMethod.valueOf(httpMethod);
         requestUrl = serviceAddress.concat(id);
         requestAsString = "";
     }
 
-//    @When("^I send request with content type \"([^\"]*)\" to the service$")
-    public void I_send_request_to_the_service(String contentType) {
+    @When("I send request with content type {string} to the service")
+    public void i_send_request_with_content_type_to_the_service(String contentType) {
         ResponseEntity<String> response = commonUtility.processHttpRequest(requestType, requestAsString, requestUrl, contentType);
         retrieveResponseBodyAndStatusCode(response);
     }
 
-//    @Then("^the retrieved body should contains the brandName \"([^\"]*)\" and the model \"([^\"]*)\" and the status code \"([^\"]*)\"$")
-    public void the_retrieved_body_should_match_with_the_status_code(String firstName, String lastName, String expectedStatusCode) {
+    @Then("the retrieved body should contains the brandName {string} and the model {string} and the status code {string}")
+    public void the_retrieved_body_should_contains_the_brand_name_and_the_model_and_the_status_code(String firstName, String lastName, String expectedStatusCode) {
         Assert.assertEquals(expectedStatusCode, responseStatusCode);
         Assert.assertTrue(responseBody.contains(firstName));
         Assert.assertTrue(responseBody.contains(lastName));
     }
 
 //    @Given("^the REST service is available and the \"([^\"]*)\" method is supported$")
-    public void the_REST_service_is_available_and_the_method_is_supported(String httpMethod) {
-        requestType = HttpMethod.valueOf(httpMethod);
-        requestUrl = serviceAddress;
-    }
-
+//    public void the_REST_service_is_available_and_the_method_is_supported(String httpMethod) {
+//        requestType = HttpMethod.valueOf(httpMethod);
+//        requestUrl = serviceAddress;
+//    }
+//
 //    @When("^I send \"([^\"]*)\" as a command body with content type \"([^\"]*)\" to the service$")
-    public void I_send_as_a_command_body_to_the_service(String fileName, String contentType) throws Throwable {
-        requestAsString = "{\"firstName\": \"George\",\"lastName\": \"Harrison\",\"role\": \"teacher\"}";
-
-        ResponseEntity<String> response = commonUtility.processHttpRequest(requestType, requestAsString, requestUrl, contentType);
-        retrieveResponseBodyAndStatusCode(response);
-    }
-
+//    public void I_send_as_a_command_body_to_the_service(String fileName, String contentType) throws Throwable {
+//        requestAsString = "{\"firstName\": \"George\",\"lastName\": \"Harrison\",\"role\": \"teacher\"}";
+//
+//        ResponseEntity<String> response = commonUtility.processHttpRequest(requestType, requestAsString, requestUrl, contentType);
+//        retrieveResponseBodyAndStatusCode(response);
+//    }
+//
 //    @Then("^the retrieved status code is \"([^\"]*)\"$")
-    public void the_retrieved_status_code_is(String expectedStatusCode) {
-        Assert.assertEquals(expectedStatusCode, responseStatusCode);
-    }
+//    public void the_retrieved_status_code_is(String expectedStatusCode) {
+//        Assert.assertEquals(expectedStatusCode, responseStatusCode);
+//    }
 
     private void retrieveResponseBodyAndStatusCode(ResponseEntity<String> response) {
         if (response != null) {
@@ -79,5 +82,4 @@ public class StepDefsImplementation {
             log.info("Status code: " + responseStatusCode);
         }
     }
-
 }
