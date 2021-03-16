830ed5468d1a708d0faf15fbbdc55f7fae39c648
MABOBROW <maciej.bobrowski@capgemini.com>
2021-03-16 10:17:02 +0100

    Update.

--- src/integrationtests/java/com/capgemini/demo/carrental/stepdefs/StepDefsImplementation.java
+++ src/integrationtests/java/com/capgemini/demo/carrental/stepdefs/StepDefsImplementation.java
@@ -1,10 +1,10 @@
 package com.capgemini.demo.carrental.stepdefs;
 
 import static com.capgemini.demo.carrental.util.ConstantUtils.API_V1;
-import static com.capgemini.demo.carrental.util.ConstantUtils.CAR_SERVICE_ENDPOINT;
 import static com.capgemini.demo.carrental.util.ConstantUtils.LOCAL_HOST;
 import static com.capgemini.demo.carrental.util.ConstantUtils.LOCAL_HOST_PORT;
 
+import com.google.common.collect.ImmutableMap;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -17,6 +17,9 @@ import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -30,36 +33,50 @@ public class StepDefsImplementation {
     private static final Logger LOGGER = LoggerFactory.getLogger(StepDefsImplementation.class);
     private static final String CAR_SERVICE_ADDRESS = LOCAL_HOST
             .concat(LOCAL_HOST_PORT)
-            .concat(API_V1)
-            .concat(CAR_SERVICE_ENDPOINT);
+            .concat(API_V1);
+
+    ImmutableMap<String, String> immutableMap = ImmutableMap.<String, String>builder()
+            .put("car", "car/")
+            .put("cars available", "car?available=true")
+            .put("rental", "rental/")
+            .put("rental search", "rental/search/")
+            .put("rental search by car", "rental/search/byCar/")
+            .put("rental search by tenant", "rental/search/byTenant/")
+            .put("tenant", "tenant/")
+            .put("remove rental", "rental/remove/")
+            .build();
 
     private HttpMethod requestType;
+    private String requestAsString;
     private String requestBody;
     private String requestUrl;
     private String responseStatusCode;
     private String responseBody;
+    private int id;
 
     @Autowired
     private RestTemplateUtils restTemplateUtils;
 
-    @Given("the REST service with initial car data id {string} is available and the {string} method is supported")
-    public void the_rest_service_with_initial_car_data_id_is_available_and_the_method_is_supported(String id, String httpMethod) {
+    //---------------Checking the correctness of the GET query
+    @Given("the REST service with initial {string} data id {string} is available and the {string} method is supported")
+    public void the_rest_service_with_initial_car_data_id_is_available_and_the_method_is_supported(String endpoint, String id, String httpMethod) {
         requestType = HttpMethod.valueOf(httpMethod);
-        requestUrl = CAR_SERVICE_ADDRESS.concat("/").concat(id);
-        requestBody = "";
+        requestUrl = CAR_SERVICE_ADDRESS.concat(immutableMap.get(endpoint)).concat("/").concat(id);
+        requestAsString = "";
     }
 
     @When("I send request with content type {string} to the service")
     public void i_send_request_with_content_type_to_the_service(String contentType) {
-        ResponseEntity<String> response = restTemplateUtils.processHttpRequest(requestType, requestBody, requestUrl, contentType);
+        ResponseEntity<String> response = restTemplateUtils.processHttpRequest(requestType, requestAsString, requestUrl, contentType);
         retrieveResponseBodyAndStatusCode(response);
     }
 
-    @Then("the retrieved body should contains the brandName {string} and the model {string} and the status code {string}")
-    public void the_retrieved_body_should_contains_the_brand_name_and_the_model_and_the_status_code(String firstName, String lastName, String expectedStatusCode) {
+    @Then("the retrieved body should contains the {string} {string} and the {string} {string} and the status code {string}")
+    public void the_retrieved_body_should_contains_the_brand_name_and_the_model_and_the_status_code(String brandKey, String brandName, String modelKey, String modelName, String expectedStatusCode) throws JSONException {
         Assert.assertEquals(expectedStatusCode, responseStatusCode);
-        Assert.assertTrue(responseBody.contains(firstName));
-        Assert.assertTrue(responseBody.contains(lastName));
+        JSONObject jsonResponseBody = new JSONObject(responseBody);
+        Assert.assertEquals(brandName, jsonResponseBody.get(brandKey).toString());
+        Assert.assertEquals(modelName, jsonResponseBody.get(modelKey).toString());
     }
 
     private void retrieveResponseBodyAndStatusCode(ResponseEntity<String> response) {
--- src/integrationtests/java/com/capgemini/demo/carrental/util/ConstantUtils.java
+++ src/integrationtests/java/com/capgemini/demo/carrental/util/ConstantUtils.java
@@ -7,7 +7,5 @@ public class ConstantUtils {
 
     public static final String LOCAL_HOST = "http://localhost";
     public static final String LOCAL_HOST_PORT = ":8080";
-    public static final String API_V1 = "/api/v1";
-    public static final String CAR_SERVICE_ENDPOINT = "/car";
-
+    public static final String API_V1 = "/api/v1/";
 }
