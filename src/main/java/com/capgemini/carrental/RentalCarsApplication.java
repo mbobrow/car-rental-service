c612a33364b3fb6d48ddae1e871cf6460f24dc64
MABOBROW <maciej.bobrowski@capgemini.com>
2026-01-12 13:59:43 +0100

    dependencies versions upgrade

--- src/main/java/com/capgemini/carrental/RentalCarsApplication.java
+++ src/main/java/com/capgemini/carrental/RentalCarsApplication.java
@@ -1,11 +1,9 @@
 package com.capgemini.carrental;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication @EnableSwagger2 public class RentalCarsApplication {
+@SpringBootApplication public class RentalCarsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RentalCarsApplication.class, args);
